108945967b3d77b50e1ca95ba8e2e1ce608f5962
fabian951 <rfabianmateo@gmail.com>
2026-09-08 20:04:25 -0500

    Proyecto base Android Studio - Escondidas (Jetpack Compose) Base de la aplicacion se crearon todas las carpetas junto con sus clases

--- app/src/main/java/com/example/caliente_frio_juego/MainActivity.kt
+++ app/src/main/java/com/example/caliente_frio_juego/MainActivity.kt
@@ -4,44 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.caliente_frio_juego.sensors.ProveedorOrientacion
+import com.example.caliente_frio_juego.ui.PantallaJuego
 import com.example.caliente_frio_juego.ui.theme.Caliente_Frio_JuegoTheme
+import com.example.caliente_frio_juego.viewmodel.JuegoViewModel
+
+class FabricaJuegoViewModel(private val proveedorOrientacion: ProveedorOrientacion) :
+    ViewModelProvider.Factory {
+    override fun <T : androidx.lifecycle.ViewModel> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
+        return JuegoViewModel(proveedorOrientacion) as T
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val proveedorOrientacion = ProveedorOrientacion(applicationContext)
+
         setContent {
             Caliente_Frio_JuegoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: JuegoViewModel = viewModel(
+                    factory = FabricaJuegoViewModel(proveedorOrientacion)
+                )
+                val estadoUi by viewModel.estadoUi.collectAsState()
+
+                PantallaJuego(
+                    estadoUi = estadoUi,
+                    alIniciar = { viewModel.iniciarJuego() },
+                    alReiniciar = { viewModel.reiniciarJuego() }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Caliente_Frio_JuegoTheme {
-        Greeting("Android")
     }
 }
